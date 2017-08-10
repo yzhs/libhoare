bea4d1c4dc6c3117a4787813026eec7fec0ddd32
Colin Benner <colin@yzhs.de>
2017-08-10 11:05:06 +0200

    Adjust to CrateConfig being a HashSet<(Name, Option<InternString>)>

--- libhoare/lib.rs
+++ libhoare/lib.rs
@@ -285,8 +285,9 @@ fn if_debug<F>(cx: &mut ExtCtxt, f: F, item: Annotatable) -> Annotatable
 where
     F: Fn(&mut ExtCtxt) -> Annotatable,
 {
-    if cx.cfg().iter().any(|item| {
-        item.node == ast::MetaItemKind::Word(Symbol::intern("debug_assertions").as_str())
+    if cx.cfg().iter().any(|&(_name, opt)| match opt {
+        Some(interned_string) => interned_string == Symbol::intern("debug_assertions"),
+        None => false,
     })
     {
         f(cx)
